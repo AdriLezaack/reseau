2027f3cd4ddff19044cc2ad8db0d44f3d8434c81
Adri <adrien.lezaack@student.uclouvain.be>
2017-10-21 10:49:16 +0200

    Sender III

--- Projet1/sender.c
+++ Projet1/sender.c
@@ -33,21 +33,21 @@ int main2(void) {
         printf("Timed out.\n");
 
     return 0;
-} 
+}
 
 
 int main(int argc, char *argv[]) {
-	
+
 	if (argc < 3){
 		printf("Erreur: trop peu d'arguments.\n");
 		return EXIT_FAILURE;
 	}
-	
+
 	unsigned int port;
 	char *addres;
 	char *file = NULL;
 	int opt;
-	
+
 	while ((opt = getopt(argc, argv, "f:")) != -1) {
 		switch (opt) {
 			case 'f':
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 	}
-	
+
 	errno = 0;
 	char *endptr;
 	port = strtold(argv[optind], &endptr);
@@ -77,59 +77,135 @@ int main(int argc, char *argv[]) {
 	addres = argv[optind + 1];
 	printf("Port: %d\n", port);
 	printf("Addres: %s\n", addres);
-	
-	
-	FILE *f = NULL;
+
+	/* Resolve the hostname */
+	struct sockaddr_in6 addr;
+	const char *err = real_address(addres, &addr);
+	if (err) {
+		fprintf(stderr, "Could not resolve hostname %s: %s\n", addres, err);
+		return EXIT_FAILURE;
+	}
+
+	// Initialisation sfd
+	int sfd;
+	sfd = create_socket(NULL, -1, &addr, port); /* Connected */
+
+	if (sfd < 0) {
+		fprintf(stderr, "Failed to create the socket!\n");
+		return EXIT_FAILURE;
+	}
+	// fprinf(stdin, "Connection établie \n");
+
 	if (file != NULL){
-		f = fopen(file, "rb");
+		FILE *f = fopen(file, "rb");
 		if (f == NULL) {
 			printf("Erreur: impossible d'ouvrir le fichier de lecture.\n");
 			return EXIT_FAILURE;
 		}
-	} else {
+
+		sendfromFile(f,sfd);
+
+		if (fclose(f) == -1){
+			printf("Erreur fermeture du fichier de lecture.\n");
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		sendfromstdin(sfd);
 		//f = STDIN_FILENO;
 	}
-	
+
+	return EXIT_SUCCESS;
+}
+
+void sendfromFile(FILE *f, int sfd){
 	uint8_t window = 1;
 	uint8_t seqnum = 0;
+	char data[512];
+	char* buffer = (char *) malloc(1024);
+	size_t len = 1024;
 	//TO DO Si pas de buffer, setter window a 0.
 	while(1){
-		char *buffer = "";
-		size_t len = 0;
-		char *data = malloc(512);
 		int rd = -1;
-		if (f != NULL){
-			//lit sur un fichier
 			rd = fread(data, 1, 512, f);
-		} else {
-			rd = read(STDIN_FILENO, data, 512);
-		}
-		
 		if (rd == -1){
 				printf("Erreur de lecture.\n");
 				return EXIT_FAILURE;
-		}			
+		}
+
 		pkt_t *pkt = pkt_new();
 		pkt_set_type(pkt, 1);
-		pkt_get_tr(0);
+		pkt_set_tr(pkt,0);
 		pkt_set_window(pkt, window);
 		pkt_set_seqnum(pkt, seqnum);
 		//pkt_set_length //Fait implicitement dans ce qui suit.
 		if (pkt_set_payload(pkt, data, rd) != PKT_OK){
 			pkt_set_tr(pkt, 1); //Tronquer le paquet
-		}	
-		free(data);
-	
+		}
+
 		pkt_encode(pkt, buffer, &len);
+
 		//write(sfd, buffer, len);
-		
+		if(write(sfd, buffer,len)){
+			fprintf(stderr, "sender(): Impossible d'envoyer le packet\n%s\n", strerror(errno));
+			exit(-1);
+		}
 		seqnum ++;
+		len = 1024;
 	}
-	
-	if (fclose(f) == -1){
-		printf("Erreur fermeture du fichier de lecture.\n");
-		return EXIT_FAILURE;
+}
+void sendfromstdin(int sfd){
+	struct timeval tv;
+	fd_set readfds,fd;
+
+	tv.tv_sec = 2;
+	tv.tv_usec = 500000;
+
+	FD_ZERO(&fd);
+	FD_SET(STDIN, &fd);
+
+	uint8_t window = 1;
+	uint8_t seqnum = 0;
+	//TO DO Si pas de buffer, setter window a 0.
+	char data[512];
+	char* buffer = (char *) malloc(1024);
+	size_t len = 1024;
+
+	while(1){
+		readfds = fd;
+		char *buffer = "";
+		size_t len = 0;
+		int rd = -1;
+		if(select(sfd+1, &readfds, NULL, NULL, &tv) == -1){
+				fprintf(stderr, "sender(): erreur dans select\n "
+						"L'erreur est %s\n", strerror(errno));
+				exit(-1);
+		}
+		rd = read(STDIN_FILENO, data, 512);
+
+		if (rd == -1){
+				printf("Erreur de lecture.\n");
+				return EXIT_FAILURE;
+		}
+
+		pkt_t *pkt = pkt_new();
+		pkt_set_type(pkt, 1);
+		pkt_set_tr(pkt,0);
+		pkt_set_window(pkt, window);
+		pkt_set_seqnum(pkt, seqnum);
+		//pkt_set_length //Fait implicitement dans ce qui suit.
+		if (pkt_set_payload(pkt, data, rd) != PKT_OK){
+			pkt_set_tr(pkt, 1); //Tronquer le paquet
+		}
+
+		pkt_encode(pkt, buffer, &len);
+
+		//write(sfd, buffer, len);
+		if(write(sfd, buffer,len)){
+			fprintf(stderr, "sender(): Impossible d'envoyer le packet\n%s\n", strerror(errno));
+			exit(-1);
+		}
+		seqnum ++;
+		len = 1024;
 	}
-	
-	return EXIT_SUCCESS;
 }
