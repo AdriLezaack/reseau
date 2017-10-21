311ccb2c395d6ac06e1c4e87e9c277cf910d81c3
Adri <adrien.lezaack@student.uclouvain.be>
2017-10-21 10:45:19 +0200

    Sender II

--- Projet1/receiver.c
+++ Projet1/receiver.c
@@ -59,26 +59,50 @@ int main(int argc, char *argv[]){
 		}
 	}
 	
+	uint8_t window = 1;
+	uint8_t seqnum = 0;
 	while(1){
 		char *buffer = "";
 		const size_t len = 0;
+		size_t lenbuffer = 0;
 		pkt_t *pkt = pkt_new();
 		if (pkt_decode(buffer, len, pkt) == PKT_OK){
 			//Si le paquet est valide...
-			int wr = -1;
-			if (f != NULL) {
-				wr = fwrite(pkt_get_payload(pkt), 1, pkt_get_length(pkt), f);
-					
-			} else {
-				wr = write(STDOUT_FILENO, pkt_get_payload(pkt), pkt_get_length(pkt));
+			if (pkt_get_type(pkt) == 1){
+				//Si le type est 1...
+				if (pkt_get_seqnum(pkt) == seqnum){
+					//Si le packet a bien un seqnum attendu
+					pkt_t *ack = pkt_new();
+					if (pkt_get_tr(pkt) == 0){
+						//Si le paquet a un payload...
+						int wr = -1;
+						if (f != NULL) {
+							wr = fwrite(pkt_get_payload(pkt), 1, pkt_get_length(pkt), f);
+						
+						} else {
+							wr = write(STDOUT_FILENO, pkt_get_payload(pkt), pkt_get_length(pkt));
+						}
+						if(wr != 1) {
+							printf("Erreur d'ecriture.\n");
+							return EXIT_FAILURE;
+						}
+						//Envoit un ack
+						pkt_set_type(ack, 1);
+						pkt_set_tr(ack, 0);
+						pkt_set_window(ack, window);
+						pkt_set_seqnum(ack, pkt_get_seqnum(pkt) + 1);
+					} else {
+						//Sinon envoit un non-ack
+						pkt_set_type(ack, 3);
+						pkt_set_tr(ack, 0);
+						pkt_set_window(ack, window);
+						pkt_set_seqnum(ack, pkt_get_seqnum(pkt));
+					}
+					pkt_encode(ack, buffer, &lenbuffer);
+				}
 			}
-			if(wr != 1) {
-				printf("Erreur d'ecriture.\n");
-				return EXIT_FAILURE;
-			}
-		} else {
-			//Sinon ignore-le.
 		}
+		//Sinon ignore-le.
 	}
 	
 	if (fclose(f) == -1){
